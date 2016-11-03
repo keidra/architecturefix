fd1368ac2e617d722947ec4550756e212d9b285d
keidra <keidrabaldwin@gmail.com>
2016-11-03 13:14:46 -0500

    adding corrections to modals

--- components/photos.php
+++ components/photos.php
@@ -40,7 +40,7 @@
           <h2 class="info">landscape design</h2>
         </div>
       </div>
-    </div><img class="img-responsive" src="img/land.jpg"></div>
+    </div><a data-target="#modal3" data-toggle="modal"><img class="img-responsive" src="img/land.jpg"></a></div>
   </div>
   <div class="row">
     <div class="col-md-4 photo line">
@@ -112,6 +112,34 @@
         <div class="row">
           <div class="col-md-7 col-md-offset-1">
             <img src="img/i1.png" class="img-responsive imgsize img2">
+            <hr class="modal-line">
+          </div>
+          <div class="col-md-12 modal-info">
+            <h3 class="modal-text">Location: Amsterdam, Netherlands</h3>
+            <h3 class="modal-text">Architect: <a class="links" href="http://www.studioninedots.nl/" target="_blank">Studioninedots</a></h3>
+            <h3 class="modal-text">Photo: <a class="links" href="http://www.petercuypers.nl/" target="_blank">Peter Cuypers</a></h3>
+            <h3 class="modal-text"><a class="links" href="http://www.studioninedots.nl/projects/161_kavel1" target="_blank">View Project</a></h3>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer"></div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<!-- Modal #3 -->
+<div aria-labelledby="gridSystemModalLabel" class="modal fade" id="modal3" role="dialog" tabindex="-1">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+        <h2 class="modal-title" id="gridSystemModalLabel">Villa Kavel 1</h2>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-7 col-md-offset-1">
+            <img src="img/land.jpg" class="img-responsive imgsize img2">
             <hr class="modal-line">
           </div>
           <div class="col-md-12 modal-info">
--- thanks.php
+++ thanks.php
@@ -24,52 +24,29 @@
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <?php include("components/nav.php"); ?><!-- JUMBOTRON - Curated. -->
-   <!--  <div class="jumbotron head">
-        <div class="container"><img alt="heading" class="img-responsive curateimg" src="img/curate.png"></div>
-    </div> -->
+    <?php include("components/nav.php"); ?>
 
-     <!-- CONTACT ME -->
-  <div class="container contact">
-    <form action="https://formspree.io/architecturefix@gmail.com" id="contact" method="post" name="contact">
-        <h3>Contact</h3>
-        <fieldset>
-            <input autofocus="" name="name" placeholder="Your name" required="" tabindex="1" type="text">
-        </fieldset>
-        <fieldset>
-            <input name="_replyto" placeholder="Your Email Address" required="" tabindex="2" type="email">
-        </fieldset>
-        <fieldset>
-            <textarea name="message" placeholder="Type your Message Here...." required="" tabindex="5"></textarea>
-        </fieldset>
-        <fieldset>
-            <input name="_next" type="hidden" value="thanks.php">
-        </fieldset>
-        <fieldset>
-            <input name="_gotcha" style="display:none" type="text">
-        </fieldset>
-        <fieldset>
-            <button data-submit="...Sending" id="contact-submit" name="submit" type="submit" value="SEND">Submit</button>
-        </fieldset>
-    </form>
-</div>
-
-
-
-
+    <!-- JUMBOTRON - Curated. -->
+    <div class="jumbotron head">
+        <div class="container">
+            <img alt="heading" class="img-responsive curateimg" src="img/curate.png">
+            <hr>
+            <h3 class="about">Thanks! We will be in touch soon!</h3>
+            <hr>
+        </div>
+    </div>        
 
 
     <footer>
         <p class="foot">&copy; ARCHITECTURE FIX 2016</p>
-    </footer><!-- /container --><!-- SCRIPTS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js">
-    </script> 
+    </footer>
+
+    <!-- SCRIPTS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script> 
     <script>
     window.jQuery || document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"><\/script>')
     </script> 
-    <script src="js/vendor/bootstrap.min.js">
-    </script> 
-    <script src="js/main.js">
-    </script> <!-- SCRIPTS -->
+    <script src="js/vendor/bootstrap.min.js"></script> 
+    <script src="js/main.js"></script>
 </body>
 </html>
